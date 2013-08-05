108b4ed3dc0f554ce93afbc55f02df448ddc919e
Zach Karpinski <zkarpinski@gmail.com>
2013-08-05 15:43:04 -0500

    Can now filter suppliers; Comment form field added; Fixed model accessibles

--- app/controllers/spree/admin/suppliers_controller.rb
+++ app/controllers/spree/admin/suppliers_controller.rb
@@ -3,15 +3,28 @@ module Spree
     class SuppliersController < ResourceController
       before_filter :copy_rtf, only: [:create, :update]
 
-     def copy_rtf
-       if params["rtf_template"]
-         params[:supplier] = { rtf_template: "" }
+      def index
+        params[:q] ||= {}
 
-         params["rtf_template"].tempfile.each_line do |l|
-           params[:supplier][:rtf_template] += l
-         end
-       end
-     end 
+        @search = Spree::Supplier.accessible_by(current_ability, :index).ransack(params[:q])
+        @suppliers = @search.result.includes([:supplier_contacts]).
+          page(params[:page]).
+          per(params[:per_page] || Spree::Config[:orders_per_page]).
+          order("name asc")
+
+        respond_with(@suppliers)        
+      end
+
+      def copy_rtf
+        if params["rtf_template"]
+          params[:supplier] = { rtf_template: "" }
+
+          params["rtf_template"].tempfile.each_line do |l|
+            params[:supplier][:rtf_template] += l
+          end
+        end
+      end 
+
     end
   end
 end
