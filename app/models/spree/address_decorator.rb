a4bf604ff471273ff1a686a3ad8db07d2fc79619
Zach Karpinski <zkarpinski@gmail.com>
2013-08-23 17:06:14 -0500

    Address also uses is_dropship when finding records

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -6,6 +6,6 @@ Spree::Address.class_eval do
       variant_ids.push(l.variant_id)
     end
   
-    find_by_sql("select a.* from spree_addresses a, spree_orders o, spree_line_items l where (a.id = o.ship_address_id or a.id = o.bill_address_id) and o.completed_at is not null and o.state = 'complete' and l.order_id = o.id and l.variant_id in (#{variant_ids.join(", ")}) order by a.lastname asc, a.firstname asc limit 100")  
+    find_by_sql("select a.* from spree_addresses a, spree_orders o, spree_line_items l where (a.id = o.ship_address_id or a.id = o.bill_address_id) and o.is_dropship = true and o.completed_at is not null and o.state = 'complete' and l.order_id = o.id and l.variant_id in (#{variant_ids.join(", ")}) order by a.lastname asc, a.firstname asc limit 100")  
   end
 end
