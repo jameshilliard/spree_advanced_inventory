b17a301160ed7fd13aee398f2775b07efe5d8efe
Zach Karpinski <zkarpinski@gmail.com>
2013-08-23 13:54:49 -0500

    Receiving stock happens inside POLineItem instead of ManageStockController

--- app/models/spree/purchase_order_line_item.rb
+++ app/models/spree/purchase_order_line_item.rb
@@ -19,6 +19,13 @@ class Spree::PurchaseOrderLineItem < ActiveRecord::Base
 
   def receive(qty_recv)
     received_purchase_order_line_items.create(quantity: qty_recv, received_at: Time.now)
+    receive_total = qty_recv
+
+    if variant.on_hand > 0
+      receive_total = variant.on_hand + qty_recv
+    end
+
+    variant.update_attributes({ on_hand: receive_total})
   end
 
   def line_total
