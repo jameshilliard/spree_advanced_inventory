e9e9d0b7ae56866e268fcb87894ea07cbf367f2e
Zach Karpinski <zkarpinski@gmail.com>
2013-08-23 14:14:34 -0500

    Fixed table name in migration

--- db/migrate/20130823184715_add_is_dropship_to_orders.rb
+++ db/migrate/20130823184715_add_is_dropship_to_orders.rb
@@ -1,5 +1,5 @@
 class AddIsDropshipToOrders < ActiveRecord::Migration
   def change
-    add_column :orders, :is_dropship, :boolean, default: false
+    add_column :spree_orders, :is_dropship, :boolean, default: false
   end
 end
