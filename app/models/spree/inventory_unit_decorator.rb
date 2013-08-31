79393800d0a2beb4f8e28cc00d96b96f1c0eb684
Zach Karpinski <zkarpinski@gmail.com>
2013-08-31 15:54:25 -0500

    set_is_dropship returns true to allow inventory units to save

--- app/models/spree/inventory_unit_decorator.rb
+++ app/models/spree/inventory_unit_decorator.rb
@@ -4,35 +4,36 @@ Spree::InventoryUnit.class_eval do
   before_validation :set_is_dropship
 
   def set_is_dropship
-    self.is_dropship = order.is_dropship
+    self.is_dropship = order.is_dropship||false
+    return true
   end
   
-  def self.decrease(order, variant, quantity)
+  def self.decrease(o, v, q)
 
     # Do not recreate stock levels for dropships
-    if self.track_levels?(variant) and not order.is_dropship
-      variant.increment!(:count_on_hand, quantity)
+    if self.track_levels?(v) and not o.is_dropship
+      v.increment!(:count_on_hand, q)
     end
 
     if Spree::Config[:create_inventory_units]
-      destroy_units(order, variant, quantity)
+      destroy_units(o, v, q)
     end
   end
   
   private
-    def self.determine_backorder(order, variant, quantity)
+    def self.determine_backorder(o, v, q)
 
-      if order.is_dropship
+      if o.is_dropship
         # Dropships immediately create stock for use with this order
-        if self.track_levels?(variant)
-          variant.increment!(:count_on_hand, quantity)
+        if self.track_levels?(v)
+          v.increment!(:count_on_hand, q)
         end
         0
       else
-        if variant.on_hand == 0 
-          quantity
-        elsif variant.on_hand.present? and variant.on_hand < quantity 
-          quantity - (variant.on_hand < 0 ? 0 : variant.on_hand)
+        if v.on_hand == 0 
+          q
+        elsif v.on_hand.present? and v.on_hand < q 
+          q - (v.on_hand < 0 ? 0 : v.on_hand)
         else
           0
         end
