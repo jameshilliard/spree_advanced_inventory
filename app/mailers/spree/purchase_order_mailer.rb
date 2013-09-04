2143d4ecbe72b71a911805ec204ab690474a3a78
Zach Karpinski <zkarpinski@gmail.com>
2013-09-04 13:40:46 -0500

    Setup to receive multiple addresses in one field

--- app/mailers/spree/purchase_order_mailer.rb
+++ app/mailers/spree/purchase_order_mailer.rb
@@ -4,11 +4,22 @@ module Spree
     def email_supplier(purchase_order, resend = false)
       @purchase_order = @po = purchase_order
 
-      subject = "[#{Spree::Config[:site_name]}] New #{@po.po_type} ##{@po.number}"
+      if resend
+        type = "Resend"
+
+      else 
+        type = "New"
+      end
+
+      subject = "[#{Spree::Config[:site_name]}] #{type} #{@po.po_type} ##{@po.number}"
       sleep 1
       ext = @po.hardcopy_extension
       attachments["#{@po.number}.#{ext}"] = File.join(Rails.root,"tmp",@po.number + ".#{ext}")
-      mail(:to => @po.supplier.email, :cc => @po.supplier_contact.email, :bcc => @po.user.email, :reply_to => @po.user.email, :subject => subject)
+      mail(:to => @po.supplier.email.split(";"), 
+           :cc => @po.supplier_contact.email.split(";"), 
+           :bcc => [@po.user.email, "zach@800ceoread.com"], 
+           :reply_to => @po.user.email, 
+           :subject => subject)
 
     end
   end
