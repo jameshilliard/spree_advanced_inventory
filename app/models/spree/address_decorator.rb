7349bcfdc6f164a9aa69a649ee11dcc276862cba
Zach Karpinski <zkarpinski@gmail.com>
2013-09-09 10:12:30 -0500

    Improved the parenthesis in address selector

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -3,13 +3,13 @@ Spree::Address.class_eval do
     find_by_sql(["select a.* 
                  from spree_addresses a, spree_orders o, spree_line_items l 
                  where 
-                 (o.is_dropship = true and 
+                 o.is_dropship = true and 
                  o.completed_at is not null and 
                  o.state = 'complete' and 
                  l.order_id = o.id and
-                 (a.id = o.ship_address_id or a.id = o.bill_address_id) and 
+                 ((a.id = o.ship_address_id or a.id = o.bill_address_id) and 
                  l.variant_id in (select distinct(variant_id) from spree_purchase_order_line_items where purchase_order_id = ?) or 
-                 a.id = ? 
+                 a.id = ?)
                  group by a.id 
                  order by a.lastname asc, a.firstname asc", po.id, po.address_id])  
   end
