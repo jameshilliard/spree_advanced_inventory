d55905787981e0bd54d33757478197fa9862ad47
Zach Karpinski <zkarpinski@gmail.com>
2013-09-09 09:57:29 -0500

    Using SQL to select valid variants instead of an inserted array

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,22 +1,16 @@
 Spree::Address.class_eval do
   def self.eligible_for_po(po)
-    v = Array.new
-    
-    po.purchase_order_line_items.each do |l|
-      v.push(l.variant_id)
-    end
-  
     find_by_sql(["select a.* 
                  from spree_addresses a, spree_orders o, spree_line_items l 
                  where 
-                 ((a.id = o.ship_address_id or a.id = o.bill_address_id) and 
-                 o.is_dropship = true and 
+                 (o.is_dropship = true and 
                  o.completed_at is not null and 
                  o.state = 'complete' and 
-                 l.order_id = o.id and 
-                 l.variant_id in (#{v.join(", ")})) or 
+                 l.order_id = o.id and
+                 (a.id = o.ship_address_id or a.id = o.bill_address_id) and 
+                 l.variant_id in (select distinct(variant_id) from spree_purchase_order_line_items where purchase_order_id = ?) or 
                  a.id = ? 
                  group by a.id 
-                 order by a.lastname asc, a.firstname asc", po.address_id])  
+                 order by a.lastname asc, a.firstname asc", po.id, po.address_id])  
   end
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -26,18 +26,19 @@ Spree::Order.class_eval do
   end
 
   def self.eligible_for_po(po)
-    variant_ids = Array.new
-    
-    po.purchase_order_line_items.each do |l|
-      variant_ids.push(l.variant_id)
-    end
-
     dropship_check = ""
 
     if po.dropship
       dropship_check = "o.is_dropship = true and"
     end
   
-    find_by_sql("select o.* from spree_orders o, spree_line_items l where o.completed_at is not null and #{dropship_check} o.state = 'complete' and l.order_id = o.id and l.variant_id in (#{variant_ids.join(", ")}) order by o.completed_at desc limit 100")
+    find_by_sql(["select o.* 
+                from spree_orders o, spree_line_items l 
+                where o.completed_at is not null and #{dropship_check} 
+                o.state = 'complete' and 
+                l.order_id = o.id and 
+                l.variant_id in (select distinct(variant_id) from spree_purchase_order_line_items where purchase_order_id = ?) 
+                order by 
+                o.completed_at desc limit 100", po.id])
   end
 end
