9e28dfacd972c9767e54afc293659b95df0897d6
Zach Karpinski <zkarpinski@gmail.com>
2013-09-12 11:14:20 -0500

    Refined PO line item details

--- app/helpers/spree/admin/purchase_orders_helper.rb
+++ app/helpers/spree/admin/purchase_orders_helper.rb
@@ -2,17 +2,29 @@ module Spree::Admin::PurchaseOrdersHelper
   def line_item_details(line_items)
     item_list = ""
 
+    po = line_items.first.purchase_order
+
     unless line_items.size == 0
+      item_list = "<span class='small'>"
       line_items.each do |l|
         sku = l.variant.sku
-        if l.purchase_order.status == "Submitted" and not l.purchase_order.dropship
+        if po.can_be_received? and l.status != "Complete"
           sku = link_to("<em>Receive #{sku}</em>?".html_safe, admin_stock_update_path(variant_id: l.variant.id))
+        else
+          sku = link_to(sku, edit_admin_product_variant_path(product_id: l.variant.product, id: l.variant), target: "_BLANK")
         end
-        item_list += "(#{l.quantity} @ #{number_to_currency(l.price)}) &nbsp; #{l.variant.product.name.split(":").first} &mdash; #{sku}<br/>".html_safe
+        item_list += "(#{l.quantity}) &nbsp; #{l.variant.product.name.split(":").first} &mdash; #{sku}<br/>".html_safe
+      end
+
+      if po.can_be_received?
+        item_list += "#{link_to 'Receive Whole PO?', admin_receive_entire_po_path(po)}".html_safe
       end
     end
+
+    item_list += "</span><br/>".html_safe
 
     return item_list
   end
 
 end
+
