8257f1e4c1191d3dfd2a60b37a999a38c6b79e7b
Zach Karpinski <zkarpinski@gmail.com>
2013-09-16 13:12:10 -0500

    Not showing line item if there is no variant

--- app/helpers/spree/admin/purchase_orders_helper.rb
+++ app/helpers/spree/admin/purchase_orders_helper.rb
@@ -7,13 +7,16 @@ module Spree::Admin::PurchaseOrdersHelper
     unless line_items.size == 0
       item_list = "<span class='small'>"
       line_items.each do |l|
-        sku = l.variant.sku
-        if po.can_be_received? and l.status != "Complete"
-          sku = link_to("<em>Receive #{sku}</em>?".html_safe, admin_stock_update_path(variant_id: l.variant.id))
-        else
-          sku = link_to(sku, edit_admin_product_variant_path(product_id: l.variant.product, id: l.variant), target: "_BLANK")
+
+        if l.variant and l.variant.try(:sku)
+          sku = l.variant.sku
+          if po.can_be_received? and l.status != "Complete"
+            sku = link_to("<em>Receive #{sku}</em>?".html_safe, admin_stock_update_path(variant_id: l.variant.id))
+          else
+            sku = link_to(sku, edit_admin_product_variant_path(product_id: l.variant.product, id: l.variant), target: "_BLANK")
+          end
+          item_list += "(#{l.quantity}) &nbsp; #{l.variant.product.name.split(":").first} &mdash; #{sku}<br/>".html_safe
         end
-        item_list += "(#{l.quantity}) &nbsp; #{l.variant.product.name.split(":").first} &mdash; #{sku}<br/>".html_safe
       end
 
       if po.can_be_received?
