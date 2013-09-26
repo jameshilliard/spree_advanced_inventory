d0d9c27e818c0a0c8207c868daffed055ec98a20
Zach Karpinski <zkarpinski@gmail.com>
2013-09-25 23:05:12 -0500

    Moving dropship field

--- app/overrides/add_is_dropship_to_admin_order_details.rb
+++ app/overrides/add_is_dropship_to_admin_order_details.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
   virtual_path: "spree/admin/orders/_form",
-  insert_after: "[data-hook='admin-order-edit-slug']",
+  insert_top: "[data-hook='admin-order-edit-slug']",
   name: "add_is_dropship_field_to_admin_order_details",
   partial: "spree/admin/orders/is_dropship_field")
