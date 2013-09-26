ef77bdcad0f8ff9f7389039b5fa45c5e860a1c7b
Zach Karpinski <zkarpinski@gmail.com>
2013-09-25 20:58:17 -0500

    Variants now work around the quantity update bug when stock is negative

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,13 +2,24 @@ Spree::Variant.class_eval do
   has_many :purchase_order_line_items
 
   def receive_quantity(num)
-    if count_on_hand > 0
-      self.on_hand += num
-    else
-      self.on_hand = num
-    end
+    num = num.to_i
 
+    if on_hand < 0
+      diff = num + on_hand
+      
+      self.on_hand = diff
+      self.save
+
+      if diff > 0
+        self.on_hand = num - diff
+        self.save
+
+      end
+    else
+      increment!(:count_on_hand, num)
+    end    
     self.save
   end
+
 end
 
