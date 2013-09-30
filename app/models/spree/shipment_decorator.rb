cdfe2292cdc7ac0cfb243896968d59e2ebc75fbe
Zach Karpinski <zkarpinski@gmail.com>
2013-09-30 11:16:28 -0500

    Reverting this change

--- app/models/spree/shipment_decorator.rb
+++
@@ -1,8 +0,0 @@
-Spree::Shipment.class_eval do
-  def determine_state(order)
-    return 'pending' unless order.can_ship?
-    return 'pending' if inventory_units.any? &:backordered?
-    return 'shipped' if inventory_units.pluck(:state).uniq == ["shipped"]
-    order.paid? ? 'ready' : 'pending'
-  end
-end
