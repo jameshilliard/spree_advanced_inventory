8d86ddc4cd8026418a97ad511157ef8e71e96025
Zach Karpinski <zkarpinski@gmail.com>
2013-10-03 14:56:45 -0500

    Not e-mailing completed dropships

--- app/mailers/spree/purchase_order_mailer.rb
+++ app/mailers/spree/purchase_order_mailer.rb
@@ -3,38 +3,42 @@ module Spree
 
     def completed_notice(po)
       @po = po
-      @completed_at = po.updated_at.strftime("%m/%d/%Y %l:%M %P")
-      
-      unless Rails.env == "development"
-        cc_list = ["mel@800ceoread.com"]
-      end
-      mail(to: [(@po.user.email ? @po.user.email : from_address)], 
-           cc: cc_list,
-           from: from_address, 
-           subject: "[#{po.number}] Purchase order received at #{@completed_at}") 
-    end
 
-    def email_supplier(purchase_order, resend = false)
-      @purchase_order = @po = purchase_order
+      unless @po.dropship
+        @completed_at = po.updated_at.strftime("%m/%d/%Y %l:%M %P")
+        
+        unless Rails.env == "development"
+          cc_list = ["mel@800ceoread.com"]
+        end
 
-      if resend
-        type = "Resend"
-
-      else 
-        type = "New"
+        mail(to: [(@po.user.email ? @po.user.email : from_address)], 
+             cc: cc_list,
+             from: from_address, 
+             subject: "[#{po.number}] Purchase order received at #{@completed_at}") 
       end
 
-      subject = @po.email_subject || "[#{Spree::Config[:site_name]}] #{type} #{@po.po_type} ##{@po.number}"
-      sleep 1
-      ext = @po.hardcopy_extension
-      attachments["#{@po.number}.#{ext}"] = File.read(File.join(Rails.root,"tmp",@po.number + ".#{ext}"))
-      mail(:to => @po.supplier.email.split(";"), 
-           :from => from_address,
-           :cc => @po.supplier_contact.email.split(";"), 
-           :bcc => [@po.user.email], 
-           :reply_to => @po.user.email, 
-           :subject => subject)
+      def email_supplier(purchase_order, resend = false)
+        @purchase_order = @po = purchase_order
 
+        if resend
+          type = "Resend"
+
+        else 
+          type = "New"
+        end
+
+        subject = @po.email_subject || "[#{Spree::Config[:site_name]}] #{type} #{@po.po_type} ##{@po.number}"
+        sleep 1
+        ext = @po.hardcopy_extension
+        attachments["#{@po.number}.#{ext}"] = File.read(File.join(Rails.root,"tmp",@po.number + ".#{ext}"))
+        mail(:to => @po.supplier.email.split(";"), 
+             :from => from_address,
+             :cc => @po.supplier_contact.email.split(";"), 
+             :bcc => [@po.user.email], 
+             :reply_to => @po.user.email, 
+             :subject => subject)
+
+      end
     end
   end
 end
