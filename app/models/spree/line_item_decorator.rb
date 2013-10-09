d0119573aaafb064def295c0ffe78ec54389a938
Zach Karpinski <zkarpinski@gmail.com>
2013-10-09 14:40:21 -0500

    Switched to use_stock method of orders

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,6 @@
 Spree::LineItem.class_eval do
   def sufficient_stock?
-    return true if Spree::Config[:allow_backorders] or order.is_dropship
+    return true if Spree::Config[:allow_backorders] or not order.use_stock
     if new_record? || !order.completed?
       variant.on_hand >= quantity
     else
