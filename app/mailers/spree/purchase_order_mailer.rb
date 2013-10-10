4daa2fda518e22df50aa3e7ee0e3ebb70cce6188
Zach Karpinski <zkarpinski@gmail.com>
2013-10-10 11:04:11 -0500

    Fixing stupid bug with emails

--- app/mailers/spree/purchase_order_mailer.rb
+++ app/mailers/spree/purchase_order_mailer.rb
@@ -16,29 +16,30 @@ module Spree
              from: from_address, 
              subject: "[#{po.number}] Purchase order received at #{@completed_at}") 
       end
+    end
 
-      def email_supplier(purchase_order, resend = false)
-        @purchase_order = @po = purchase_order
 
-        if resend
-          type = "Resend"
+    def email_supplier(purchase_order, resend = false)
+      @purchase_order = @po = purchase_order
 
-        else 
-          type = "New"
-        end
+      if resend
+        type = "Resend"
 
-        subject = @po.email_subject || "[#{Spree::Config[:site_name]}] #{type} #{@po.po_type} ##{@po.number}"
-        sleep 1
-        ext = @po.hardcopy_extension
-        attachments["#{@po.number}.#{ext}"] = File.read(File.join(Rails.root,"tmp",@po.number + ".#{ext}"))
-        mail(:to => @po.supplier.email.split(";"), 
-             :from => from_address,
-             :cc => @po.supplier_contact.email.split(";"), 
-             :bcc => [@po.user.email], 
-             :reply_to => @po.user.email, 
-             :subject => subject)
-
+      else 
+        type = "New"
       end
+
+      subject = @po.email_subject || "[#{Spree::Config[:site_name]}] #{type} #{@po.po_type} ##{@po.number}"
+      sleep 1
+      ext = @po.hardcopy_extension
+      attachments["#{@po.number}.#{ext}"] = File.read(File.join(Rails.root,"tmp",@po.number + ".#{ext}"))
+      mail(:to => @po.supplier.email.split(";"), 
+           :from => from_address,
+           :cc => @po.supplier_contact.email.split(";"), 
+           :bcc => [@po.user.email], 
+           :reply_to => @po.user.email, 
+           :subject => subject)
+
     end
   end
 end
