1755c54c85f11e513cb9a62cf912e6e66b87a84e
Zach Karpinski <zkarpinski@gmail.com>
2013-11-20 15:43:51 -0600

    Explicitly building order state for quotes, dropships and inventory adjustments

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,9 +1,22 @@
 Spree::Admin::OrdersController.class_eval do
   def new
     @order = Spree::Order.create
+
     if params[:is_quote]
       @order.update_column(:is_quote, true)
+      @order.update_column(:inventory_adjusted, false)
+
+    elsif params[:is_dropship]
+      @order.update_column(:is_dropship, true)
+      @order.update_column(:inventory_adjusted, false)
+
+    else
+      @order.update_column(:is_dropship, false)
+      @order.update_column(:is_quote, false)
+      @order.update_column(:inventory_adjusted, true)
+
     end
+
     respond_with(@order)
   end  
 end
