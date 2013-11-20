c81194112c32d0b157d27d357a5492dc9fba545f
Zach Karpinski <zkarpinski@gmail.com>
2013-11-20 15:43:06 -0600

    Sorting variants by id descending

--- app/controllers/spree/variants_controller_decorator.rb
+++ app/controllers/spree/variants_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Admin::VariantsController.class_eval do
   def search
     search_params = { :product_name_cont => params[:q], :sku_cont => params[:q] }
-    @variants = Spree::Variant.where(deleted_at: nil, is_master: false).ransack(search_params.merge(:m => 'or')).result
+    @variants = Spree::Variant.where(deleted_at: nil, is_master: false).order("id desc").ransack(search_params.merge(:m => 'or')).result
   end
 end
