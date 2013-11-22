6d9aaae35755813ecfd3a62b3ce90b470d93b35c
Zach Karpinski <zkarpinski@gmail.com>
2013-11-22 11:58:41 -0600

    Tests for PO completion refactoring

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -4,6 +4,16 @@ describe Spree::Order do
   let(:order) { stub_model(Spree::Order) }
   let(:inventory_unit) { stub_model(Spree::InventoryUnit) }
   let(:inventory2) { stub_model(Spree::InventoryUnit) }
+  let(:payment1) { stub_model(Spree::Payment) }
+  let(:payment2) { stub_model(Spree::Payment) }
+  let(:payment3) { stub_model(Spree::Payment) }
+  let(:payment4) { stub_model(Spree::Payment) }
+  let(:shipping_method) { mock_model Spree::ShippingMethod, :calculator => mock('calculator'), :adjustment_label => "Shipping" }
+  let(:shipment) do
+    shipment = Spree::Shipment.new :order => order, :shipping_method => shipping_method
+    shipment.state = 'pending'
+    shipment
+  end  
 
   before :each do
     @variant1 = mock_model(Spree::Variant, 
@@ -24,6 +34,24 @@ describe Spree::Order do
     order.stub(:line_items => @line_items)
     order.stub(:inventory_units => [inventory_unit])
     order.stub(:variant_inventory_units => [inventory_unit])
+    
+    payment1.stub(:state).and_return("pending")
+    payment1.stub(:source_type).and_return("Spree::CreditCard")
+    payment1.stub(:amount).and_return(10.50)
+   
+    payment2.stub(:state).and_return("checkout")
+    payment2.stub(:source_type).and_return(nil)
+    payment2.stub(:amount).and_return(5.25)
+    
+    payment3.stub(:state).and_return("paid")
+    payment3.stub(:source_type).and_return("Spree::CreditCard")
+    payment3.stub(:amount).and_return(6.25)
+
+    payment4.stub(:state).and_return("paid")
+    payment4.stub(:source_type).and_return(nil)
+    payment4.stub(:amount).and_return(7.25)
+
+    order.stub(:payments).and_return([payment1, payment2, payment3, payment4])      
   end
 
   context "#dropship_conversion" do
@@ -203,6 +231,92 @@ describe Spree::Order do
       order.update_inventory_unit(inventory_unit, "sold")
     end
   end
+
+  context "#pending_credit_card_payment_total" do
+    it "should sum all credit card amounts" do
+      order.pending_credit_card_payment_total.should == payment1.amount
+    end
+  end
+
+  context "#pending_credit_card_payment_total" do
+    it "should sum all check payment amounts" do
+      order.pending_check_payment_total.should == payment2.amount
+    end
+
+  end
+
+  context "#pending_payments" do
+    it "should return pending payments" do
+      order.pending_payments.should == [payment1, payment2]
+    end
+  end
+
+  context "#try_to_capture_payment" do
+    before :each do
+      order.stub(:staff_comments => "")
+      order.stub(:update_staff_comments).and_return(true)
+      order.stub(:total).and_return(payment1.amount + payment2.amount)
+
+      payment1.stub(:capture!).and_return(true)
+      payment1.stub(:save).and_return(true)
+
+      payment2.stub(:capture!).and_return(true)
+      payment2.stub(:save).and_return(true)
+    
+    end
+
+    it "should total credit_card payments" do
+      order.should_receive(:pending_credit_card_payment_total)
+      order.try_to_capture_payment
+    end
+
+    it "should total check payments" do
+      order.should_receive(:pending_check_payment_total)
+      order.try_to_capture_payment
+    end
+
+    context "order total matches payments total" do
+      it "should capture! payment" do
+        payment1.should_receive(:capture!)
+        payment1.should_receive(:save)
+        
+        payment2.should_receive(:capture!)
+        payment2.should_receive(:save)
+        
+        order.try_to_capture_payment
+      end
+    end
+
+    context "order total does not match payments total" do
+      before :each do 
+        order.stub(:total).and_return(payment1.amount)
+      end
+
+      it "should not capture payments and then update staff comments" do
+        payment1.should_not_receive(:capture!)
+        payment2.should_not_receive(:capture!)
+        order.should_receive(:update_staff_comments).with("Payment totals did not match order total")
+      end
+    end
+  end
+
+  context "#try_to_ship_shipments" do
+
+  end
+  
+  context "#update_staff_comments" do
+    it "should update staff_comments without callbacks if column is available" do
+      order.stub(:staff_comments => "")
+      order.should_receive(:update_attributes_without_callbacks).with({ :staff_comments => "\ntesting\n"})
+      order.update_staff_comments("testing")
+    end
+
+    it "should not update staff_comments if column is unavailable" do
+      order.should_not_receive(:update_attributes_without_callbacks)
+      order.update_staff_comments("testing")
+    end
+  end
+
 
 end
 
