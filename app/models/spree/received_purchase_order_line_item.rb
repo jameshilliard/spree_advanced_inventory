33be3aa7ff8e73ab60a59260ad0b4bdf30963d18
Zach Karpinski <zkarpinski@gmail.com>
2013-12-17 15:11:11 -0600

    Accessible purchase_order_line_item_id

--- app/models/spree/received_purchase_order_line_item.rb
+++ app/models/spree/received_purchase_order_line_item.rb
@@ -1,5 +1,9 @@
 class Spree::ReceivedPurchaseOrderLineItem < ActiveRecord::Base
   belongs_to :purchase_order_line_item
-  attr_accessible :quantity, :received_at
+  attr_accessible :quantity, :received_at, :purchase_order_line_item_id
+
+  def purchase_order
+    purchase_order_line_item.purchase_order
+  end
 
 end
