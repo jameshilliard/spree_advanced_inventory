f48bb92124cd8d4a5ee332bbbd3884729a16da5e
Zach Karpinski <zkarpinski@gmail.com>
2014-01-09 17:28:40 -0600

    Using SQL directly to make sure updates are not lost when multiple people edit orders for the same variant

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -33,6 +33,20 @@ Spree::Variant.class_eval do
     return p
   end
 
+  def increment!(attribute, by = 1)
+    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
+    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
+    self.class.update_all("#{attribute} = #{original_value_sql} + #{by.to_i}", "id = #{id}")
+    reload
+  end
+
+  def decrement!(attribute, by = 1)
+    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
+    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
+    self.class.update_all("#{attribute} = #{original_value_sql} - #{by.to_i}", "id = #{id}")
+    reload
+  end
+
   private
 
     def process_backorders
