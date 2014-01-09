1dc2e75bcb570630144ca48e60334c528f519904
Zach Karpinski <zkarpinski@gmail.com>
2014-01-09 17:28:48 -0600

    Re-enabling button

--- app/overrides/add_is_dropship_to_admin_order_details.rb
+++ app/overrides/add_is_dropship_to_admin_order_details.rb
@@ -2,4 +2,4 @@ Deface::Override.new(
   virtual_path: "spree/admin/orders/_form",
   insert_top: "[data-hook='admin-order-edit-slug']",
   name: "add_is_dropship_field_to_admin_order_details",
-  partial: "spree/admin/orders/is_dropship_field", disabled: true)
+  partial: "spree/admin/orders/is_dropship_field")
