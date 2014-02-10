d798ced1ae2c5cb71216d03e7472404ae960e2d7
Zach Karpinski <zkarpinski@gmail.com>
2014-02-10 12:38:01 -0600

    JS to warn when unsaved changes will be lost

--- app/assets/javascripts/admin/spree_advanced_inventory.js
+++ app/assets/javascripts/admin/spree_advanced_inventory.js
@@ -1,1 +1,28 @@
-//= require admin/spree_core
+$(document).ready(function () {
+  var unsaved = false;
+
+  $(":input").change(function(){ 
+    unsaved = true;
+  });
+
+  // Another way to bind the event
+  $(window).bind('beforeunload', function() {
+      if(unsaved){
+          return "You have unsaved changes on this page. Do you want to leave this page and discard your changes or stay on this page?";
+      }
+  });
+
+  // Monitor dynamic inputs
+  $(document).on('change', ':input', function(){ 
+      unsaved = true;
+  });
+
+  $('.btn-success').click(function() {
+      unsaved = false;
+  });
+
+  $('.pagination a').click(function() {
+      unsaved = false;
+  });
+
+});
