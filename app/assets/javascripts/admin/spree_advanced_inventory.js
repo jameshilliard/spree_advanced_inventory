07c15ce9b9a2f8ad19d6d9da705895334fd1f556
Zach Karpinski <zkarpinski@gmail.com>
2014-02-11 15:32:32 -0600

    Dynamic input checks only

--- app/assets/javascripts/admin/spree_advanced_inventory.js
+++ app/assets/javascripts/admin/spree_advanced_inventory.js
@@ -2,18 +2,12 @@ $(document).ready(function () {
   var unsaved = false;
   var check_for_unsaved_changes = false;
 
-  $(":input").change(function(){ 
-    unsaved = true;
-  });
-
-  // Another way to bind the event
   $(window).bind('beforeunload', function() {
     if(unsaved) {
       return "You have unsaved changes on this page. Do you want to leave this page and discard your changes or stay on this page?";
     }
   });
 
-  // Monitor dynamic inputs
   $(document).on('change', ':input', function() {
     if(check_for_unsaved_changes) {
       unsaved = true;
