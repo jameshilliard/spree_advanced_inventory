5bd628e64ff6f2c242ff8d0ccc1e7e80fa268222
Zach Karpinski <zkarpinski@gmail.com>
2014-03-18 15:46:20 -0500

    Wrapping queries in transactions

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -109,16 +109,20 @@ Spree::Variant.class_eval do
   end
 
   def increment!(attribute, by = 1)
-    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
-    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
-    self.class.update_all("#{attribute} = #{original_value_sql} + #{by.to_i}", "id = #{id}")
+    Spree::Variant.transaction do
+      raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
+      original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
+      self.class.update_all("#{attribute} = #{original_value_sql} + #{by.to_i}", "id = #{id}")
+    end
     reload
   end
 
   def decrement!(attribute, by = 1)
-    raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
-    original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
-    self.class.update_all("#{attribute} = #{original_value_sql} - #{by.to_i}", "id = #{id}")
+    Spree::Variant.transaction do
+      raise ArgumentError("Invalid attribute: #{attribute}") unless attribute_names.include?(attribute.to_s)
+      original_value_sql = "CASE WHEN #{attribute} IS NULL THEN 0 ELSE #{attribute} END"
+      self.class.update_all("#{attribute} = #{original_value_sql} - #{by.to_i}", "id = #{id}")
+    end
     reload
   end
 
@@ -146,21 +150,23 @@ Spree::Variant.class_eval do
             new_level -= backordered_units.length
           end
 
-          if order_list.size > 0
-            order_list.each do |oid|
-              o = Spree::Order.find(oid)
-              o.shipments.each do |shipment|
+          Spree::Order.transaction do
+            if order_list.size > 0
+              order_list.each do |oid|
+                o = Spree::Order.find(oid)
+                o.shipments.each do |shipment|
 
-                unless shipment.state == "shipped"
-                  shipment.update!(o)
+                  unless shipment.state == "shipped"
+                    shipment.update!(o)
+                  end
                 end
-              end
 
-              o.updater.update_shipment_state
-              o.update_attributes_without_callbacks({ :shipment_state => o.shipment_state })
+                o.updater.update_shipment_state
+                o.update_attributes_without_callbacks({ :shipment_state => o.shipment_state })
+              end
             end
+            self.update_attribute_without_callbacks(:count_on_hand, new_level)
           end
-          self.update_attribute_without_callbacks(:count_on_hand, new_level)
         end
       end
     end    
