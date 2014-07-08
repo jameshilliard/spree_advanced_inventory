cf8b09960e044a701fbe10216ee6a231ee4dc9ab
James Hilliard <james.hilliard1@gmail.com>
2014-07-07 22:14:35 -0500

    change v1 to stock items

--- db/migrate/20140303205124_create_full_inventory_view.rb
+++ db/migrate/20140303205124_create_full_inventory_view.rb
@@ -3,7 +3,7 @@ class CreateFullInventoryView < ActiveRecord::Migration
     execute("
 create view full_inventory as 
 
-select v1.id, v1.sku, v1.cost_price, v1.count_on_hand, reserved_units, v1.name as title, v1.permalink 
+select v1.id, v1.sku, v1.cost_price, spree_stock_items.count_on_hand, reserved_units, v1.name as title, v1.permalink 
 from (select spree_variants.id, spree_variants.sku, spree_variants.cost_price, spree_stock_items.count_on_hand, spree_products.name, spree_products.permalink from spree_variants, spree_products where spree_variants.is_master = false and spree_products.id = spree_variants.product_id) v1
 left join (select variant_id, count(id) as reserved_units from spree_inventory_units where state = 'sold' and is_dropship = false and is_quote = false group by variant_id) i
 on v1.id = i.variant_id
